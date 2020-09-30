932d826aba3e3fa88935433d6962ae2812740ce3
CheesyRaven <mmcfarland.it@gmail.com>
2020-09-30 01:00:11 -0700

    Added counter source code

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h3>Hello World</h3>
+		<Counter
+			:year="2020"
+			:month="9"
+			:date="31"
+			:hour="10"
+			:minute="10"
+			:second="10"
+			:millisecond="10"
+		/>
+	</div>
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+export default {
+	components: {
+		Counter
+	}
+
+};
+
+</script>
+
+<style lang="scss" scoped>
+</style>
